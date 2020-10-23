caad3b685d3d01a1ae1d916c0b098a18fc42f321
Gabriel Santos <gabrielrsantoss@icloud.com>
2020-10-23 15:00:40 -0300

    Refac: pass components creation responsability to ContactComponent

--- src/components/Contact/ContactComponent.jsx
+++ src/components/Contact/ContactComponent.jsx
@@ -1,11 +1,27 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import ContactModal from 'components/ContactModal';
 
-const ContactComponent = ({ identity, contact, createModal }) => {
+const ContactComponent = ({
+    identity,
+    contact,
+    createModal,
+    show_contact_modal,
+    destroyModal
+}) => {
     return (
-        <div className="Contact" onClick={createModal}>
-            <i className="fas fa-user" />
-            <p>{contact && contact.name ? contact.name : identity}</p>
+        <div>
+            <div className="Contact" onClick={createModal}>
+                <i className="fas fa-user" />
+                <p>{contact && contact.name ? contact.name : identity}</p>
+            </div>
+            {show_contact_modal && (
+                <ContactModal
+                    show={show_contact_modal}
+                    closeModal={destroyModal}
+                    contact={contact}
+                />
+            )}
         </div>
     );
 };
@@ -13,7 +29,9 @@ const ContactComponent = ({ identity, contact, createModal }) => {
 ContactComponent.propTypes = {
     identity: PropTypes.string.isRequired,
     contact: PropTypes.object.isRequired,
-    createModal: PropTypes.func.isRequired
+    createModal: PropTypes.func.isRequired,
+    show_contact_modal: PropTypes.bool.isRequired,
+    destroyModal: PropTypes.func.isRequired
 };
 
 export default ContactComponent;
--- src/components/Contact/ContactContainer.js
+++ src/components/Contact/ContactContainer.js
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { getContactAsync } from 'services/contact-service';
 import { withLoading } from 'services/common-service';
-import ContactModal from 'components/ContactModal';
 import ContactComponent from './ContactComponent';
 
 const ContactContainer = ({ identity }) => {
@@ -18,20 +17,13 @@ const ContactContainer = ({ identity }) => {
     const destroyModal = () => setShowContactModal(false);
 
     return (
-        <div>
-            <ContactComponent
-                identity={identity}
-                contact={contact}
-                createModal={createModal}
-            />
-            {show_contact_modal && (
-                <ContactModal
-                    show={show_contact_modal}
-                    closeModal={destroyModal}
-                    contact={contact}
-                />
-            )}
-        </div>
+        <ContactComponent
+            identity={identity}
+            contact={contact}
+            createModal={createModal}
+            show_contact_modal={show_contact_modal}
+            destroyModal={destroyModal}
+        />
     );
 };
 
