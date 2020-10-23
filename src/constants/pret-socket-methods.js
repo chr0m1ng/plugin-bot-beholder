a13452ffd1ae3065e5835b7258e45b0124a3bc99
Gabriel Santos <gabrielrsantoss@icloud.com>
2020-10-23 16:21:58 -0300

    Add: pret websocket service

---
+++ src/constants/pret-socket-methods.js
@@ -0,0 +1,9 @@
+class PretSocketMethods {
+    static answer_command = 'AnswerCommand';
+
+    static send_command = 'SendCommand';
+
+    static trace = 'trace';
+}
+
+export default PretSocketMethods;
